25b13cec6725f46152195836d1ddeee1fd07c801
Kyle <Polysplitter@gmail.com>
2018-11-24 21:31:44 -0600

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/polysplitter/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git@github.com:polysplitter/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
